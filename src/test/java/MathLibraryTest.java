0ce49fddf5559831ff73b994c56c32baf148f3ce
Oksana Kostina <oksana2109k@rambler.ru>
2025-05-28 19:51:57 +0400

    -

--- src/test/java/MathLibraryTest.java
+++ src/test/java/MathLibraryTest.java
@@ -1,39 +1,41 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
 
 public class MathLibraryTest {
 
     @Test
     public void testGetFactorial() {
-        Assertions.assertEquals(3628800, MathLibrary.getFactorial(10));
-        Assertions.assertThrows(RuntimeException.class, () -> {
+        assertEquals(MathLibrary.getFactorial(10), 3628800);
+        assertThrows(RuntimeException.class, () -> {
             MathLibrary.getFactorial(13);
         });
     }
 
     @Test
     public void testGetTriangle() {
-        Assertions.assertEquals(6, MathLibrary.getTriangle(3,4,5));
-        Assertions.assertThrows(RuntimeException.class, () -> {
+        assertEquals(MathLibrary.getTriangle(3,4,5), 6);
+        assertThrows(RuntimeException.class, () -> {
             MathLibrary.getTriangle(3,4,500);
         });
     }
 
     @Test
     public void testArithmetic() {
-        Assertions.assertEquals(11, MathLibrary.addition(5, 6));
-        Assertions.assertEquals(-1, MathLibrary.subtraction(5, 6));
-        Assertions.assertEquals(30, MathLibrary.multiplication(5, 6));
-        Assertions.assertEquals(1, MathLibrary.delete(5, 5));
-        Assertions.assertThrows(RuntimeException.class, () -> {
+        assertEquals(MathLibrary.addition(5, 6), 11);
+        assertEquals(MathLibrary.subtraction(5, 6), -1);
+        assertEquals(MathLibrary.multiplication(5, 6), 30);
+        assertEquals(MathLibrary.delete(5, 5), 1);
+        assertThrows(RuntimeException.class, () -> {
             MathLibrary.delete(5, 0);
         });
     }
 
     @Test
     public void testComparison() {
-        Assertions.assertEquals("a больше b", MathLibrary.comparison(6, 5));
-        Assertions.assertEquals("b больше a", MathLibrary.comparison(4, 5));
-        Assertions.assertEquals("числа равны", MathLibrary.comparison(5, 5));
+        assertEquals(MathLibrary.comparison(6, 5), "a больше b");
+        assertEquals(MathLibrary.comparison(4, 5), "b больше a");
+        assertEquals(MathLibrary.comparison(5, 5), "числа равны");
     }
 }
